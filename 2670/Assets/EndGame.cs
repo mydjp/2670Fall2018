ada0f3e0acdf4f6b3f0abc102398643de3c04b2a
Unknown <mydjp0912@yahoo.com>
2018-08-29 13:15:45 -0600

    Uggg

---
+++ 2670/Assets/EndGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+
+	public FloatData Health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+
+	{
+		if (Health.Value < 0);
+		//end Game
+	}
+}
---
+++ 2670/Assets/KillPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+
+	public FloatData Health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Health.Value <= 0)
+		{
+			Destroy(gameObject);
+		}
+		
+	}
+}
