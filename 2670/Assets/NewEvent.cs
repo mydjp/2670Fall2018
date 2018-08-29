71658aff38cf1423cac799356315decdc71f32e0
Unknown <mydjp0912@yahoo.com>
2018-08-29 13:49:12 -0600

    Mess with events

---
+++ 2670/Assets/NewEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NewEvent : MonoBehaviour 
+{
+
+    
+    
+
+    public UnityEvent Event;
+    
+        private void OnMouseDown()
+        {
+            Event.Invoke();
+        }
+    
+    
+}
