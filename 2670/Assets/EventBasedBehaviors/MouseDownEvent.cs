2505ecd8bc12f4e5dbcd2ee3bb47dd21a59627f9
Unknown <mydjp0912@yahoo.com>
2018-08-29 14:27:49 -0600

    Added alot of general Events GOOD FOR EVERYTHING

---
+++ 2670/Assets/EventBasedBehaviors/MouseDownEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseDownEvent : MonoBehaviour
+{
+
+
+    public UnityEvent Event;
+
+    private void OnMouseDown()
+    {
+        Event.Invoke();
+    }
+}
