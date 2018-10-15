83a1a803805d3019d79c6cd8b4adc5da029c6fc0
Unknown <mydjp0912@yahoo.com>
2018-10-15 14:42:43 -0600

    Working on store

---
+++ CasualGame/Casual/Assets/Scripts/Store/Objects.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "object",menuName = "Store/Objects")]
+public class Objects : ScriptableObject
+{
+    public List<Object> ObjectList;
+
+}
