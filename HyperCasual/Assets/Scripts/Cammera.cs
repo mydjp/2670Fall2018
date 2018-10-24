6d06de1a427f17bc0c29c1688c27ed4e5b4f2011
Unknown <mydjp0912@yahoo.com>
2018-10-24 11:31:41 -0600

    got movement working

---
+++ HyperCasual/Assets/Scripts/Cammera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cammera : MonoBehaviour
+{
+
+	public float moveDistance = 10;
+
+
+	void Update()
+	{
+
+		Vector3 pos = transform.position;
+
+		if (Input.GetKeyDown("a"))
+		{
+
+			pos.z += moveDistance;
+
+		}
+
+
+
+
+		if (Input.GetKeyDown("d"))
+		{
+
+			pos.z += moveDistance;
+
+			transform.position = pos;
+
+		}
+	}
+}
---
+++ HyperCasual/Assets/Scripts/Move.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float moveDistance = 10;
+
+	public int aCount = 0;
+	public int dCount = 1;
+
+	void Update () {
+		
+		Vector3 pos = transform.position;
+
+		if(Input.GetKeyDown("a") && aCount == 0)
+		{
+			aCount = 1;
+			pos.x -= moveDistance;
+			pos.z += moveDistance;
+			dCount = 0;
+			
+		}
+
+		if(Input.GetKeyDown("a") && aCount == 1)
+		{
+			aCount = 1;
+			pos.z += moveDistance;
+			dCount = 0;
+
+			
+		}
+
+
+
+
+		if(Input.GetKeyDown("d") && dCount == 0)
+		{
+			dCount = 1;
+			pos.x += moveDistance;
+			pos.z += moveDistance;
+			aCount = 0;
+		}
+
+		if(Input.GetKeyDown("d") && dCount == 1)
+		{
+			dCount = 1;
+			pos.z += moveDistance;
+			aCount = 0;
+		}
+		
+		transform.position = pos;
+
+	}
+}
